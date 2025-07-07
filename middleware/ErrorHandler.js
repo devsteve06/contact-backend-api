3524998c25d8aeec80f849911b020fc715774f26
devsteve06 <eusteve906@gmail.com>
2025-07-07 12:28:10 +0300

    feat : Error Handling Update - Created a file  constants file to store relevant status codes and imported the file into  ErrorHandler file for efficient error handling. - Installed express-async-handler  to aid in future error handling where asynca-await is applied.

--- middleware/ErrorHandler.js
+++ middleware/ErrorHandler.js
@@ -1,15 +1,39 @@
+const { constants } = require("../constants");
+
 const errorHandler = (err,req,res,next) =>{
      const statusCode = res.statusCode ? res.statusCode : 500  //returns internal server error if status code unavailable
-     switch (statusCode) {
-        case 400:
-            res.json({ "title" : "Not found", message : err.message, stackTrace : err.stack})
-                    }
+    
+     //switch case for different error scenarios
+     switch (constants) {
+        case constants.VALIDATION_ERROR:
+            res.json({title: "Validation Error",
+                 message : err.message,
+                  stackTrace : err.stack})
             break;
-     
+        case constants.UNATHORIZED:
+            res.json({title: "Unauthorized", 
+                message : err.message, 
+                stackTrace : err.stack})
+            break; 
+        case constants.FORBIDDEN_ERROR:
+            res.json({title: "Forbidden", 
+                message : err.message, 
+                stackTrace : err.stack})
+            break; 
+        case constants.NOT_FOUND:
+            res.json({title: "Not Found", 
+                message : err.message, 
+                stackTrace : err.stack})
+            break; 
+        case constants.SERVER_ERROR:
+            res.json({title: "Server Error", 
+                message : err.message, 
+                stackTrace : err.stack})
+            break;  
         default:
+            console.log('No errors, all good.');
             break;
      }
-     //formatting errors in json
-     
+    }
 
 module.exports = errorHandler
