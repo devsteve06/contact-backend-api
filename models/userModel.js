228a26890dce6d1abd7ab46502789c39b416b60c
devsteve06 <eusteve906@gmail.com>
2025-07-16 01:34:33 +0300

    feat: Add user model with schema validation:
    - Username, email and password are required.
    - The unique property of the user email ensures no user duplication while the minlength of 6 ensures that increased  complexity of passwords.

---
+++ models/userModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, 'Please add a username']
+    },
+    email: {    
+        type: String,
+        required: [true, 'Please add an email'],
+        unique: true  
+    },
+    password: { 
+        type: String,
+        required: [true, 'Please add a password'],
+        minlength: 6
+    }
+}, {
+    timestamps: true
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const errorHandler = require('./middleware/ErrorHandler')
 const connectDb = require('./config/dbConfig')
 require('dotenv').config()
+
 const app  = express()
 
 const port = process.env.PORT || 5000
