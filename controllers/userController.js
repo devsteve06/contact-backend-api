c42be2ac3b7b0ed5a33ebf82d417de3575401b9e
devsteve06 <eusteve906@gmail.com>
2025-07-16 02:38:18 +0300

    feat: user registration logic implementation:
    - The registration endpoint logic has been implemented together with
     relevant errohandling logic.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,48 @@
 const asyncHandler = require('express-async-handler')
+const user = require('../models/userModel')
+const bcrypt = require('bcrypt')
+
 
 //@desc register user
 //@route POST /api/users/register
 //@access public
 const registerUser = asyncHandler(async (req,res)=>{
-    res.json({message:"succcessfully accessed user register endpoint"})
-}) 
+    const {username,email,password} = req.body;
+
+    if(!username || !email || !password){
+        res.status(400)
+        throw new Error("Please fill all the fields")
+    }
+
+    const userExists = await user.findOne({email})
+    if(userExists){
+        res.status(400)
+        throw new Error("User already exists")
+    }       
+
+    const hashedPassword = await bcrypt.hash(password,10)
+    console.log(hashedPassword)
+
+    const newUser = await user.create({
+        username,
+        email,
+        password:hashedPassword
+    })
+
+    console.log(newUser)
+
+    if(newUser){    
+    res.status(201).json({
+        _id: newUser.id,
+        username: newUser.username,
+        email: newUser.email 
+    })
+    }else{
+        res.status(400)
+        throw new Error("Invalid user data")
+    }
+})
+   
 
 //@desc login user
 //@route POST /api/users/login
